899e5c0559229f13af18b87995422586772abc15
Sxilverr <silverthemusician1@gmail.com>
2024-03-11 10:51:51 -0400

    movement and stats completed

--- LABYRINTH/Assets/Player/Scripts/Movement.cs
+++ LABYRINTH/Assets/Player/Scripts/Movement.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed;
+    public static float speed;
+    public bool doublejump;
+    public float dtg;
+    public Rigidbody2D rb;
+    public static float jump;
+    public float ff;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        dtg = GetComponent<Collider2D>().bounds.extents.y;
     }
 
     // Update is called once per frame
@@ -20,5 +26,36 @@ public class Movement : MonoBehaviour
         if(Input.GetKey(KeyCode.A)){
             transform.position -= new Vector3(speed/2500f,0,0);
         }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(new Vector3(0, -ff, 0));
+        }
+    }
+    void Update()
+    {
+        ff = jump / 2;
+        if (jumpenabled() && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector3(0, 4*jump, 0));
+        }
+        if (!jumpenabled() && Input.GetKeyDown(KeyCode.Space) && doublejump == true)
+        {
+            rb.velocity=(new Vector3(rb.velocity.x, jump/10f, 0));
+            doublejump = false;
+        }
+        if (jumpenabled())
+        {
+            doublejump = true;
+        }
+    }
+    public bool jumpenabled()
+    {
+        if(Physics2D.Raycast(transform.position, -Vector3.up, dtg + 0.05f)==true)
+            return Physics2D.Raycast(transform.position, -Vector3.up, dtg + 0.05f);
+        if (Physics2D.Raycast(new Vector3(transform.position.x - 0.5f, transform.position.y, transform.position.z), -Vector3.up, dtg + 0.05f))
+            return Physics2D.Raycast(new Vector3(transform.position.x - 0.5f, transform.position.y, transform.position.z), -Vector3.up, dtg + 0.05f);
+        if (Physics2D.Raycast(new Vector3(transform.position.x + 0.5f, transform.position.y, transform.position.z), -Vector3.up, dtg + 0.05f))
+            return Physics2D.Raycast(new Vector3(transform.position.x + 0.5f, transform.position.y, transform.position.z), -Vector3.up, dtg + 0.05f);
+        return false;
     }
 }
--- LABYRINTH/Assets/Player/Scripts/StatManager.cs
+++ LABYRINTH/Assets/Player/Scripts/StatManager.cs
@@ -4,16 +4,47 @@ using UnityEngine;
 
 public class StatManager : MonoBehaviour
 {
-    public float light;
+    public float lightC;
+    public float lightCm;
+    public float lightCa;
+
     public float manaC;
+    public float manaCa;
+    public float manaCm;
+    public float manaCe;
+
     public float manaR;
+    public float manaRm;
+
     public float manaE;
+    public float manaEa;
+    public float manaEm;
+
     public float healthC;
+    public float healthCa;
+
     public float healthR;
+    public float healthRm;
+    public float healthRa;
+
     public float armor;
     public float shield;
+    public float shieldalloc;
+    public float shieldm;
+
     public float strength;
-    public float mobility;
+    public float strengthalloc;
+    public float strengthm;
+    public float strengtha;
+
+    public float speed;
+    public float jumph;
+    public float mobilitym;
+    public float mobilitya;
+    public float mobilityalloc;
+    public float speedalloc;
+    public float jumpalloc;
+
     public float drawspd;
     // Start is called before the first frame update
     void Start()
@@ -24,6 +55,17 @@ public class StatManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        lightC = (1 + lightCm) * (100 + lightCa);
+        manaC = Mathf.Pow((1 + manaCm), (1 + manaCe))*(lightC+manaCa);
+        manaR = (1 + manaRm) * manaC / 100f;
+        manaE = (1 + manaEm) * (100 + manaEa);
+        healthC = 100f + healthCa;
+        healthR = (1 + healthRm) * (Mathf.Log10(lightC - 99) + 1) * (healthC + healthRa) / 100f;
+        shield = (1 + shieldm) * shieldalloc / 10f;
+        strength = (1 + strengthm) * (strengthalloc + 100 * (1 + strengtha));
+        speed = 0.5f * (1 + mobilitym) * (100*Mathf.Log10(lightC) + mobilitya + Mathf.Pow(speedalloc, 0.6666666666667f));
+        jumph = 0.5f * (1 + mobilitym) * (100*Mathf.Log10(lightC) + mobilitya + Mathf.Pow(jumpalloc, 0.6666666666667f));
+        Movement.speed = speed*3f;
+        Movement.jump = jumph;
     }
 }
