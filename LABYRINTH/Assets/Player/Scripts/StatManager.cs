eef14f8164f6073fba4ff51b738d4946dfb103bf
Sxilverr <silverthemusician1@gmail.com>
2024-03-10 21:15:31 -0400

    Simple scripts and player added movement and stats scripts created basic sprite in basic shape added

---
+++ LABYRINTH/Assets/Player/Scripts/StatManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatManager : MonoBehaviour
+{
+    public float light;
+    public float manaC;
+    public float manaR;
+    public float manaE;
+    public float healthC;
+    public float healthR;
+    public float armor;
+    public float shield;
+    public float strength;
+    public float mobility;
+    public float drawspd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
