af503f111bb34a5cde5482dbdb6b55b44e66bc69
Sxilverr <silverthemusician1@gmail.com>
2024-04-29 11:39:41 -0400

    Lost Adventurer Behavior Start

--- LABYRINTH/Assets/Enemies/Lost Adventurer/Behavior Scripts/LostAdventurerBehavior.cs
+++ LABYRINTH/Assets/Enemies/Lost Adventurer/Behavior Scripts/LostAdventurerBehavior.cs
@@ -9,6 +9,10 @@ public class LostAdventurerBehavior : MonoBehaviour
     public Animator anim;
     public float truedist;
     public bool left;
+    public float mindist;
+    public float attackstate;
+    public float bowdist;
+    public GameObject arrow;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,28 +23,89 @@ public class LostAdventurerBehavior : MonoBehaviour
     void FixedUpdate()
     {
         truedist = Vector3.Distance(transform.position, GameObject.FindWithTag("Player").transform.position);
-        if (truedist <= followdist)
+        if(attackstate == 0) {
+            if(truedist >= bowdist && truedist <= followdist && anim.GetBool("Bowing") == false)
+            {
+                if (transform.position.x - GameObject.FindWithTag("Player").transform.position.x < 0)
+                {
+                    left = false;
+                    transform.position += new Vector3(speed / 50, 0, 0);
+                }
+                else
+                {
+                    left = true;
+                    transform.position -= new Vector3(speed / 50, 0, 0);
+                }
+                anim.SetBool("Running", true);
+                anim.SetBool("Bowing", false);
+            }
+            else
+            {
+                anim.SetBool("Running", false);
+                anim.SetBool("Bowing", true);
+                if(anim.GetCurrentAnimatorClipInfo(0)[0].clip.name == "BowFullState") {
+                    anim.SetBool("Bowing", false);
+                    if(left == false)
+                    {
+                        Instantiate(arrow, transform.position + new Vector3(0.5f, -0.0625f, 0), Quaternion.identity);
+                    } else
+                    {
+                        Instantiate(arrow, transform.position + new Vector3(-0.5f, -0.25f, 0), Quaternion.Euler(0, 0, 180));
+                    }
+                    attackstate = 1;
+                }
+            }
+        }
+        if (attackstate == 1)
+        {
+            anim.SetBool("Bowing", false);
+            if (truedist <= followdist && truedist > mindist)
+            {
+                if (transform.position.x - GameObject.FindWithTag("Player").transform.position.x < 0)
+                {
+                    left = false;
+                    transform.position += new Vector3(speed / 50, 0, 0);
+                }
+                else
+                {
+                    left = true;
+                    transform.position -= new Vector3(speed / 50, 0, 0);
+                }
+                anim.SetBool("Running", true);
+            }
+            else
+            {
+                anim.SetBool("Running", false);
+            }
+            if (truedist <= followdist + 5)
+            {
+                if (transform.position.x - GameObject.FindWithTag("Player").transform.position.x < 0)
+                {
+                    left = false;
+                }
+                else
+                {
+                    left = true;
+                }
+            }
+
+        }
+        if (truedist <= followdist + 5)
         {
             if (transform.position.x - GameObject.FindWithTag("Player").transform.position.x < 0)
             {
                 left = false;
-                transform.position += new Vector3(speed / 50, 0, 0);
             }
             else
             {
                 left = true;
-                transform.position -= new Vector3(speed / 50, 0, 0);
             }
-            anim.SetBool("Running", true);
-        }
-        else
-        {
-            anim.SetBool("Running", false);
         }
         if (left == true)
         {
             GetComponent<SpriteRenderer>().flipX = true;
-        } else
+        }
+        else
         {
             GetComponent<SpriteRenderer>().flipX = false;
         }
---
+++ LABYRINTH/Assets/Enemies/Lost Adventurer/Behavior Scripts/earrowvel.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class earrowvel : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float vel;
+    public float tick;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right * vel;
+        //Debug.Log((1 - (transform.rotation.z + 0.5f)));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag != "Enemy")
+            Destroy(gameObject);
+    }
+}
